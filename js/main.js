e6f9e8826bdd5c7cce15bcd9a7dd4680b5f5045e
AllysonDavid <allysondavid99@outlook.com>
2026-01-27 07:40:00 +0900

    Update footer centering, portfolio section, and project images

--- js/main.js
+++ js/main.js
@@ -46,12 +46,6 @@ $(document).ready(function(){
         responsive: { 0: {items: 2}, 768: {items: 4}, 900: {items: 6} }
     });
 
-    $(".testimonials-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
-    });
     
 })(jQuery);
 
